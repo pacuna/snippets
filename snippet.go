c70ea30b760e5e977540aa8dce172d04b6b7b018
Pablo Acuna <pacuna@pm.me>
2020-04-12 14:16:29 -0500

    Add gob encoding

--- snippet.go
+++ snippet.go
@@ -1,6 +1,11 @@
 package snippets
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"time"
+)
 
 type Snippet struct {
 	Id        string
@@ -9,4 +14,32 @@ type Snippet struct {
 	Content   string
 	CreatedAt time.Time
 	Tags      []string
+}
+
+func New(id string, title string, language string, content string, createdAt time.Time, tags []string) *Snippet {
+	return &Snippet{Id: id, Title: title, Language: language, Content: content, CreatedAt: createdAt, Tags: tags}
+}
+
+func Encode(s *Snippet, format string) []byte {
+
+	var b bytes.Buffer
+	if format == "gob" {
+		enc := gob.NewEncoder(&b)
+		err := enc.Encode(s)
+		if err != nil {
+			log.Fatal("encode error:", err)
+		}
+	}
+	return b.Bytes()
+}
+
+func Decode(data []byte, format string) *Snippet {
+
+	var s *Snippet
+	enc := gob.NewDecoder(bytes.NewReader(data))
+	err := enc.Decode(&s)
+	if err != nil {
+		log.Fatal("decode error:", err)
+	}
+	return s
 }
--- snippet_test.go
+++ snippet_test.go
@@ -1,1 +1,33 @@
 package snippets_test
+
+import (
+	"snippets"
+	"testing"
+	"time"
+)
+
+func TestEncodeDecodeGob(t *testing.T) {
+	var tags = []string{
+		"go",
+	}
+	s := &snippets.Snippet{
+		Id:       "0",
+		Title:    "Hello world",
+		Language: "Go",
+		Content: `
+import "fmt"
+
+func main(){
+  fmt.Println("Hello world")
+}
+`,
+		CreatedAt: time.Now(),
+		Tags:      tags,
+	}
+
+	enc := snippets.Encode(s, "gob")
+	dec := snippets.Decode(enc, "gob")
+	if dec.Content != s.Content {
+		t.Errorf("Decoded content: %s, different than original content: %s", dec.Content, s.Content)
+	}
+}
